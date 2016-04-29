4acea3f724ae62d25529f41ef4176637c387f1da
Austin Shirley <ausshir@gmail.com>
2016-04-29 00:23:55 -0600

    Extended routes to connect to the RoverProcess

--- WebUI/Routes.py
+++ WebUI/Routes.py
@@ -1,20 +1,15 @@
-from bottle import Bottle, route, get, template, static_file, post
-
+from bottle import Bottle, route, get, post, template, static_file, request, response
 import json
-import os
-from threading import Lock
-
-#global value to keep track of sent data
-outData = {}
-lock = Lock()
 
 class WebServerRoutes():
 
-	def __init__(self):
+	def __init__(self, parent=None):
 		self.val = True
 		self.instance = Bottle()
 		self.outData = {}
 		self.lock = Lock()
+		
+		self.parent = parent
 		
 		self.buildRoutes()
 		
@@ -25,11 +20,9 @@ class WebServerRoutes():
 		self.instance.route('/index', method="GET", callback=self.mainPage)
 		self.instance.route('/home', method="GET", callback=self.mainPage)
 		
-		# gamepad
+		# other pages
 		self.instance.route('/gamepad', method="GET", callback=self.gamepad)
 		self.instance.route('/gamepadoptions', method="GET", callback=self.gamepadOptions)
-		
-		# camera
 		self.instance.route('/camera', method="GET", callback=self.camera)
 		
 		# static files (TBH I have no idea how this works.. but it does!)
@@ -37,12 +30,16 @@ class WebServerRoutes():
 		self.instance.route('/favicon.ico', method="GET", callback=self.sendFavicon)
 		
 		# communication
+		self.instance.route('/data/<item>', method="POST", callback=self.recvData)
+		self.instance.route('/req/<item>', method="POST", callback=self.sendData)
 		
 	# define the template to show and any preprocessing
 	def mainPage(self):
 		return template('index')
 		
 	def gamepad(self):
+		if self.parent is not None:
+			self.parent.setShared("WebserverTest", "hello from routes!")
 		return template('gamepad')
 		
 	def camera(self):
@@ -58,35 +55,75 @@ class WebServerRoutes():
 	# Special route for Favicon	@route('/favicon.ico')
 	def sendFavicon(self):
 	    return static_file('favicon.ico', root='./WebUI/static/images/')
-		
-		
-	# Getting data from the GUI (buttons, text and controls)
-	#@post('/gamepadAxes')
-	#def showPostDbg():
-	#	message = request.json
-	#	print message
-		
+	
+	
+	''' Communication Info!
+			Note: in order to send and receive data your JS needs to look
+			something like this:
+				
+				For Sending to Rover:
 
-	#testing for communication 
-		
-	# test to send data to rover
-	#@post('/testSend')
-	def testSend():
-		global outData
-		global lock
-		message = request.json
-		print message
-		with lock:
-			for key in message: 
-					outData[key] = message[key]
-		
-	def testToUI(self, message):
-		print message
-		
-	def rvcFromUI(self):
-		with self.lock:
-			return self.outData
-		
+					$.ajax({
+						url: "/data/<data name goes here>",
+						method: "POST",
+						data: JSON.stringify({"axes" : gp.axes}),
+						contentType: "application/json"
+					});
+					
+					In this case, the JSON gets translated directly into a rover
+					dictionary object, just like in JsonServer when using a
+					standalone application. Data is then distributed by StateManager.
+					
+					The reccomended format is to collect similar messages to a specific
+					thread/module into a single key and use an array for the values.
+					
+				For Getting from Rover:
+				
+					$.ajax({
+						url: "/req/<request name goes here>",
+						method: "POST",
+						data: JSON.stringify({"axes" : gp.axes}),
+						contentType: "application/json",
+						complete: function(results) {
+								alert("Something happened!" + JSON.stringify(results));
+							}
+
+					});
+					
+					Of course in this case you will want to actually do something 
+					with the results. I don't reccomend alerts because they are annoying! ;)
+				
+	'''
+	
+	# POST Route for sending data directly to the rover database
+	#	Incoming data must be valid JSON (checked)
+	def recvData(self, item):
+		data = self.byteify(json.loads(request.body.read()))
+		if isinstance(data, dict):
+			if self.parent is not None:
+				self.parent.uplink.put(data)
+			else:
+				print data
+				
+	# POST Route for requesting data from the rover	
+	def sendData(self, item):
+		if self.parent is not None:
+			return json.dumps({item : self.parent.state[item]})
+		else:
+			return json.dumps({item : "test"})
+
+	# Utility to more robustly parse json into a python dict
+	def byteify(self, input):
+		if isinstance(input, dict):
+			return(
+					{self.byteify(key): self.byteify(value)
+					for key, value in input.iteritems()})
+		elif isinstance(input, list):
+			return [self.byteify(element) for element in input]
+		elif isinstance(input, unicode):
+			return input.encode('utf-8')
+		else:
+			return input		
 		
 		
 		
