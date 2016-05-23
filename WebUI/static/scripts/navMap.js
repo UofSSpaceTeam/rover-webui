feef5fe1bc6ee69d095bf76b446700178f86e33a
kylebonilla <kylebonilla2242@gmail.com>
2016-05-23 12:36:08 -0600

    added events to navMap
    added events to display currently selected marker data.

--- WebUI/static/scripts/navMap.js
+++ WebUI/static/scripts/navMap.js
@@ -2,6 +2,7 @@
 var roverMarker; 
 var mapGroup;
 var mapMarkers;
+var currentMarker; 
 
 function updateRoverPos() {
 	//TODO: make sure it is getting values from correct place
@@ -11,11 +12,36 @@ function updateRoverPos() {
 }
 
 function addMarker(){
-	var marker = L.marker([document.getElementById("YPos").value, document.getElementById("XPos").value]);
+	alert(document.getElementById("NewName").value);
+	var marker = L.marker([document.getElementById("NewLat").value,
+		document.getElementById("NewLng").value ], 
+		{title: document.getElementById("NewName").value});
+		
 	mapGroup.addLayer(marker);
+	
 	marker.on('click', function(e) {
-		alert(e.latlng);
-	});
+		//change marker Icon
+		currentMarker = e.target; 
+		//change marker Icon 
+		displayMakerData(); 
+	}); 
+}
+
+function displayMakerData() {
+	document.getElementById("CurrName").value = currentMarker.options.title;
+	document.getElementById("CurrLat").value = currentMarker.getLatLng().lat;
+	document.getElementById("CurrLng").value = currentMarker.getLatLng().lng;
+}
+
+function getBearing(curLat, curLng) {
+	//taken from the navigation coordinate class
+	var lat1 = curLat * Math.PI / 180;
+	var lng1 = curLng * Math.PI / 180;
+	var lat2 = currentMarker.getLatLng().lat * Math.PI / 180; 
+	var lng2 = currentMarker.getLatLng().lng * Math.PI / 180;
+	
+	var bearing = (Math.atan2(Math.sin(lon2-lon1)*Math.cos(lat2), Math.cos(lat1)*Math.sin(lat2) -Math.sin(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1)));
+	return (bearing % (2*Math.PI)) * 180 / Math.PI; 
 }
 
 function navMap() {
@@ -37,9 +63,5 @@ function navMap() {
 	map.addLayer(mapGroup);
 		
 	//TODO: the initial position should be changed to current position
-	roverMarker = L.marker([38.3730379, -110.7140391], {title: "Rover"}).addTo(map);
-	
-	roverMarker.on('click', function(e) {
-		alert(e.latlng);
-	});
+	roverMarker = L.marker([38.3730, -110.7140], {title: "Rover"}).addTo(map);
 }
