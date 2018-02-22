a6074bf57b8d89765a9e2d6257c13b4776a9cb98
BishalBSR <bishalroy98@hotmail.com>
2018-02-22 01:16:15 -0600

    Chart data updates every second from robocluster

--- vue/App.js
+++ vue/App.js
@@ -1,20 +1,19 @@
 var template =`
   <div>
     <storage-text resource='TargetReached'></storage-text>
-    <data-visualization  :data1=data2></data-visualization>
+    <data-visualization  resource1="SpeedData" resource2="FuelData"></data-visualization>
     <h1>{{ greeting }}</h1>
     <maps></maps>
   </div>
 `;
+
 var app = new Vue({
   el: '#app',
   template: template,
   data: {
-    greeting: 'Welcome to your Vue.js app!',
-    data2: {speed:60,Fuel:100}
+    greeting: 'Welcome to your Vue.js app!'
   },
   methods: {
 
   },
 })
-
