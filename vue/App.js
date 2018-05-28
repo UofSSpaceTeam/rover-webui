30c947f9a1814175c0f19e6c6807d1a34424bdab
Lars-Kish <lwk045@mail.usask.ca>
2018-05-27 19:12:26 -0600

    Reformat css

--- vue/App.js
+++ vue/App.js
@@ -1,12 +1,15 @@
 var template =`
   <div>
+
     <storage-text resource='Autopilot'></storage-text>
     <boolean-switch resource="driving"> </boolean-switch>
     <dash-cluster  resource1="Speed" resource2="current" resource3="roverHeading"></dash-cluster>
     <maps resource1="roverLat" resource2="roverLong" resource3="markerLat" resource4="markerLong" resource5="roverHeading" resource6="sendWaypoints"></maps>
+
     <gauge dataSource="Speed" minValue = "0" maxValue = "10" units="m/s"></gauge>
     <gauge dataSource="Acceleration" minValue = "50" maxValue = "100" units="m/s/s"></gauge>
     <line-plot dataSource="velocity" xlabel="time"></line-plot>
+
   </div>
 `;
 
